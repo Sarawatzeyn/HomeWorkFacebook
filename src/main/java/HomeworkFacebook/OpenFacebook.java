5023748ddda3463e691af87cee5112f4f21de75f
Sarawatzeyn <sarawat.rahman@gmail.com>
2023-09-18 00:21:09 -0400

    LoginTest

---
+++ src/main/java/HomeworkFacebook/OpenFacebook.java
@@ -0,0 +1,11 @@
+package HomeworkFacebook;
+
+import org.openqa.selenium.WebDriver;
+
+public class OpenFacebook {
+WebDriver driver;
+public void openApplication() {
+	System.out.println("Hello selenium");
+	
+}
+}
---
+++ src/test/java/loginTest/openFacebook.java
@@ -0,0 +1,36 @@
+package loginTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class openFacebook {
+	WebDriver driver;
+	@BeforeTest
+	public void openApplication() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	
+	}
+	@Test
+	public void logInTest1() {
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("NurulBhai");
+		WebElement password= driver.findElement(By.xpath("//input[@name='pass']"));
+		password.clear();
+		password.sendKeys("lala");
+driver.findElement(By.xpath("//button[@name='login']")).click();
+		
+	}
+
+}
