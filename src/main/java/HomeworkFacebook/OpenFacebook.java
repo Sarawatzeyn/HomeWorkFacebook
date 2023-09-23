f76e4700652c86b2b3fd5f9ebf79e920ee8ece7a
Sarawatzeyn <sarawat.rahman@gmail.com>
2023-09-23 08:22:52 -0400

    new

--- src/main/java/HomeworkFacebook/OpenFacebook.java
+++ src/main/java/HomeworkFacebook/OpenFacebook.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class OpenFacebook {
 WebDriver driver;
 public void openApplication() {
-	System.out.println("Hello selenium");
+	System.out.println("Hello selenium and hello");
 	
 }
 }
